55f1eb92a4ac8b64c1af9151e56a4cc9ee9a9f50
’李建博‘ <1755632546@qq.com>
2023-03-06 18:25:22 +0800

    feat: 集成调试工具

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,13 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const WebpackBar = require('webpackbar')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const { ReactInspectorPlugin } = require('react-dev-inspector/plugins/webpack')
+
 const smp = new SpeedMeasurePlugin()
 const path = require('path')
 module.exports = {
   webpack: smp.wrap({
     configure: (webpackConfig, { env, paths }) => {
-      console.log('env: ', {
-        env,
-      })
       if (
         process.env.NODE_ENV === 'production' &&
         process.env.ANALYSE === 'true'
@@ -19,12 +18,32 @@ module.exports = {
             analyzerHost: '127.0.0.1',
             analyzerPort: 8888,
             openAnalyzer: true, // 构建完打开浏览器
-            reportFilename: path.resolve(__dirname, `analyzer/index.html`),
-          }),
+            reportFilename: path.resolve(__dirname, `analyzer/index.html`)
+          })
         )
       }
       return webpackConfig
     },
-    plugins: [new WebpackBar()],
-  }),
+    plugins: [
+      new WebpackBar(),
+      new ReactInspectorPlugin()
+      // {
+      //   // react-dev-inspector的配置 目前这些配置足够点击页面直接在vscode编辑器中找到对应的文件了
+      //   //剩余的高级配置需要自己去官网上学习，
+      //   plugin: new ReactInspectorPlugin(),
+      //   options: {
+      //     excludes: ['xxx-file-will-be-exclude', /regexp-to-match-file /]
+      //   }
+      // }
+    ]
+    /** react-dev-inspector - dev server config */
+    // devServer: overrideDevServer((serverConfig) => {
+    //   // https://webpack.js.org/configuration/dev-server/#devserversetupmiddlewares
+    //   serverConfig.setupMiddlewares = (middlewares) => {
+    //     middlewares.unshift(launchEditorMiddleware)
+    //     return middlewares
+    //   }
+    //   return serverConfig
+    // })
+  })
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 // import App from './App'
 import { RouterProvider } from 'react-router-dom'
+import { Inspector } from 'react-dev-inspector'
+
 import { router } from './router'
 
 // import reportWebVitals from './reportWebVitals'
+const isDev = process.env.NODE_ENV === 'development'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -14,6 +17,14 @@ root.render(
   <>
     <RouterProvider router={router} />
     {/* <ClickToComponent editor="vscode" /> */}
+    {isDev && (
+      <Inspector
+        // props see docs:
+        // https://github.com/zthxxx/react-dev-inspector#inspector-component-props
+        keys={['control', 'shift', 'command', 'c']}
+        disableLaunchEditor={false}
+      />
+    )}
   </>
   // <App />,
   // </React.StrictMode>
